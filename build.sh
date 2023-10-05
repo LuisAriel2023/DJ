244b34bc5270d845d928b439278a7ba63cdf83eb
luisAriel <luimaanzules50@hotmail.com>
2023-10-04 22:11:07 -0500

    fist commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+#poetry install
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+#DJANGO_SUPERUSER_PASSWORD=04072003 python manage.py createsuperuser --no-input --username Rafael2  --email rokero.0407@hotmail.com
